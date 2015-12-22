86d22f6c7a8a8f6148c49c98ce61d6fd2a2b1bd8
Hunt Tang <tangmingming@sensetime.com>
2015-12-22 15:41:32 +0800

    initial commit

---
+++ src/main/java/com/sensetime/test/HttpRequestSender.java
@@ -0,0 +1,187 @@
+package com.sensetime.test;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.methods.*;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * This is a common Http Request Sender which disregards any business logic
+ *
+ * @author Hunt
+ */
+public class HttpRequestSender {
+    public enum ArgsKey {TYPE, SCHEME, HOST, PORT, PATH, PARAM, ENTITY_TYPE, ENTITY, CONTENT_TYPE}
+
+    public enum Type {GET, POST, DELETE}
+
+    /**
+     * When ArgsKey.ENTITY_TYPE is set to EntityType.MULTIPART,
+     * ArgsKey.ENTITY should be List<MultipartEntityTriplet>,
+     * and ArgsKey.CONTENT_TYPE will be disregarded.
+     */
+    public enum EntityType {
+        STRING, STREAM, BINARY, FILE, MULTIPART
+    }
+
+    private final CloseableHttpClient httpClient;
+
+    public HttpRequestSender() {
+        httpClient = HttpClientBuilder.create().build();
+        Runtime.getRuntime().addShutdownHook(new ShutdownHook());
+    }
+
+    public CloseableHttpResponse send(final HashMap<ArgsKey, Object> requestArgs) {
+        try {
+            URIBuilder uri = new URIBuilder();
+            if (requestArgs.containsKey(ArgsKey.SCHEME)) {
+                uri.setScheme((String)requestArgs.get(ArgsKey.SCHEME));
+            }
+            else {
+                uri.setScheme("http");
+            }
+            uri.setHost((String)requestArgs.get(ArgsKey.HOST));
+            uri.setPath((String)requestArgs.get(ArgsKey.PATH));
+            if (requestArgs.containsKey(ArgsKey.PORT)) {
+                uri.setPort((int)requestArgs.get(ArgsKey.PORT));
+            }
+            if (requestArgs.containsKey(ArgsKey.PARAM)) {
+                uri.setParameters((List<NameValuePair>)requestArgs.get(ArgsKey.PARAM));
+            }
+
+            Type type = (Type)requestArgs.get(ArgsKey.TYPE);
+
+            HttpUriRequest httpRequest;
+            if (type == Type.GET) {
+                httpRequest = new HttpGet(uri.build());
+            }
+            else if (type == Type.DELETE) {
+                httpRequest = new HttpDelete(uri.build());
+            }
+            else if (type == Type.POST) {
+                httpRequest = new HttpPost(uri.build());
+                if (!setEntityByType((HttpPost)httpRequest, (EntityType)requestArgs.get(ArgsKey.ENTITY_TYPE),
+                        requestArgs.get(ArgsKey.ENTITY), (ContentType)requestArgs.get(ArgsKey.CONTENT_TYPE))) {
+                    return null;
+                }
+            }
+            else {
+                return null;
+            }
+
+            CloseableHttpResponse response = httpClient.execute(httpRequest);
+
+            return response;
+        }
+        catch (URISyntaxException | IOException e) {
+        }
+
+        return null;
+    }
+
+    private boolean setEntityByType(final HttpEntityEnclosingRequestBase httpRequest, final EntityType entityType,
+                                    final Object entity, final ContentType contentType) {
+        // If none of these 3 variables is set, consider it as blank entity
+        if (entityType == null && entity == null && contentType == null) {
+            return true;
+        }
+
+        if (entityType == null || entity == null) {
+            return false;
+        }
+
+        if (entityType == EntityType.STRING) {
+            EntityBuilder entityBuilder = EntityBuilder.create().setContentType(contentType).setText((String)entity);
+            httpRequest.setEntity(entityBuilder.build());
+        }
+        else if (entityType == EntityType.STREAM) {
+            EntityBuilder entityBuilder = EntityBuilder.create().setContentType(contentType).setStream((InputStream)entity);
+            httpRequest.setEntity(entityBuilder.build());
+        }
+        else if (entityType == EntityType.BINARY) {
+            EntityBuilder entityBuilder = EntityBuilder.create().setContentType(contentType).setBinary((byte[])entity);
+            httpRequest.setEntity(entityBuilder.build());
+        }
+        else if (entityType == EntityType.MULTIPART) {
+            MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+            for (MultipartEntityTriplet triplet : (List<MultipartEntityTriplet>)entity) {
+                if (triplet.getType() == EntityType.STRING) {
+                    entityBuilder.addTextBody(triplet.getName(), (String)triplet.getValue());
+                }
+                else if (triplet.getType() == EntityType.STREAM) {
+                    entityBuilder.addBinaryBody(triplet.getName(), (InputStream)triplet.getValue());
+                }
+                else if (triplet.getType() == EntityType.BINARY) {
+                    entityBuilder.addBinaryBody(triplet.getName(), (byte[])triplet.getValue());
+                }
+                else if (triplet.getType() == EntityType.FILE) {
+                    entityBuilder.addBinaryBody(triplet.getName(), (File)triplet.getValue());
+                }
+                else {
+                    return false;
+                }
+            }
+            httpRequest.setEntity(entityBuilder.build());
+        }
+        else {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        httpClient.close();
+        super.finalize();
+    }
+
+    public static class MultipartEntityTriplet {
+        private final String name;
+        private final EntityType type;
+        private final Object value;
+
+        public MultipartEntityTriplet(final String name, final EntityType type, final Object value) {
+            if (name == null || type == null) {
+                throw new IllegalArgumentException(name + " may not be null");
+            }
+            this.name = name;
+            this.type = type;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public EntityType getType() {
+            return type;
+        }
+
+        public Object getValue() {
+            return value;
+        }
+    }
+
+    private class ShutdownHook extends Thread {
+        @Override
+        public void run() {
+            try {
+                httpClient.close();
+            }
+            catch (IOException e) {
+            }
+        }
+    }
+}
---
+++ src/main/java/com/sensetime/test/Main.java
@@ -0,0 +1,85 @@
+package com.sensetime.test;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.lingala.zip4j.exception.ZipException;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Created by Hunt on 9/1/15.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+    }
+
+    private static void testEncryptedZipFile() throws IOException, ZipException {
+        String filename = "/Users/hunt/Temp/testEncryptedZip.zip";
+
+        ZipParameters zipParam = new ZipParameters();
+        zipParam.setSourceExternalStream(true);
+        zipParam.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+        zipParam.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_ULTRA);
+        zipParam.setEncryptFiles(true);
+        zipParam.setEncryptionMethod(Zip4jConstants.ENC_METHOD_AES);
+        zipParam.setAesKeyStrength(Zip4jConstants.AES_STRENGTH_256);
+        zipParam.setPassword("sensetime_15811247942");
+
+        try (FileOutputStream fos = new FileOutputStream(filename);
+             net.lingala.zip4j.io.ZipOutputStream zos = new net.lingala.zip4j.io.ZipOutputStream(fos)) {
+            zipParam.setFileNameInZip("abc/test");
+            zos.putNextEntry(null, zipParam);
+            zos.write(new byte[1]);
+            zos.closeEntry();
+            zos.finish();
+        }
+    }
+
+    private static void testZipFile() throws IOException {
+        try (FileOutputStream fos = new FileOutputStream("temp.zip");
+             ZipOutputStream zos = new ZipOutputStream(fos)) {
+
+            ZipEntry ze = new ZipEntry("Image/imageSchema.json");
+            FileInputStream fis = new FileInputStream("/Users/hunt/Temp/imageSchema.json");
+            zos.putNextEntry(ze);
+            IOUtils.copy(fis, zos);
+            fis.close();
+
+            ze = new ZipEntry("Label/taskSchema.json");
+            fis = new FileInputStream("/Users/hunt/Temp/taskSchema.json");
+            zos.putNextEntry(ze);
+            IOUtils.copy(fis, zos);
+            fis.close();
+        }
+
+        FileUtils.forceDelete(new File("temp.zip"));
+    }
+
+    private static void testStream() {
+        final int count = 1000000;
+
+        List<Integer> list = new ArrayList<>(count);
+        Random random = new Random();
+        for (int i = 0; i < count; ++i) {
+            list.add(random.nextInt());
+        }
+
+        long start = System.nanoTime();
+        list.parallelStream().count();
+        long end = System.nanoTime();
+
+        System.out.println(TimeUnit.NANOSECONDS.toMillis(end - start));
+    }
+}
